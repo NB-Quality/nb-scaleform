fb974541b2a11e256b477a5035a3add4bfc0fb69
negbook <negbook@gmail.com>
2022-06-28 02:01:11 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+version '1.0.0'
+author 'negbook'
+description ''
+repository ''
+
+shared_scripts {
+    '@nb-loop/nb-loop.lua',
+    'nb-scaleform.lua'
+}
+
+dependencies {
+    'nb-loop'
+}
